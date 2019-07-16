64d59ad1ba655b145dbc2f3a1ac6370c1d4766df
Caleb Bauermeister <cbauerman@gmail.com>
2019-07-16 15:36:31 -0700

    Rough out showDaily chanegs

--- src/extension/features/toolkit-reports/pages/net-worth/component.jsx
+++ src/extension/features/toolkit-reports/pages/net-worth/component.jsx
@@ -5,6 +5,7 @@ import { formatCurrency } from 'toolkit/extension/utils/currency';
 import { localizedMonthAndYear, sortByGettableDate } from 'toolkit/extension/utils/date';
 import { l10n } from 'toolkit/extension/utils/toolkit';
 import { FiltersPropType } from 'toolkit-reports/common/components/report-context/component';
+import { LabeledCheckbox } from 'toolkit-reports/common/components/labeled-checkbox';
 import { Legend } from './components/legend';
 
 export class NetWorthComponent extends React.Component {
@@ -13,7 +14,9 @@ export class NetWorthComponent extends React.Component {
     allReportableTransactions: PropTypes.array.isRequired,
   };
 
-  state = {};
+  state = {
+    showDaily: false,
+  };
 
   componentDidMount() {
     this._calculateData();
@@ -29,8 +32,19 @@ export class NetWorthComponent extends React.Component {
   }
 
   render() {
+    const { showDaily } = this.state;
     return (
       <div className="tk-flex tk-flex-column tk-flex-grow">
+        <div className="tk-flex tk-pd-05 tk-border-b">
+          <div className="tk-income-breakdown__filter">
+            <LabeledCheckbox
+              id="tk-income-breakdown-hide-income-selector"
+              checked={showDaily}
+              label="Show Daily Net Worth"
+              onChange={this.toggleDaily}
+            />
+          </div>
+        </div>
         <div className="tk-flex tk-justify-content-end">
           {this.state.hoveredData && (
             <Legend
@@ -44,6 +58,12 @@ export class NetWorthComponent extends React.Component {
       </div>
     );
   }
+
+  toggleDaily = async ({ currentTarget }) => {
+    const { checked } = currentTarget;
+    await this.setState({ showDaily: checked });
+    this._calculateData();
+  };
 
   _renderReport = () => {
     const _this = this;
@@ -120,8 +140,9 @@ export class NetWorthComponent extends React.Component {
     const accounts = new Map();
     const allReportData = { assets: [], labels: [], debts: [], netWorths: [] };
     const transactions = this.props.allReportableTransactions.slice().sort(sortByGettableDate);
+    const { showDaily } = this.state;
 
-    let lastMonth = null;
+    let lastDate = null;
     function pushCurrentAccountData() {
       let assets = 0;
       let debts = 0;
@@ -136,22 +157,26 @@ export class NetWorthComponent extends React.Component {
       allReportData.assets.push(assets);
       allReportData.debts.push(debts);
       allReportData.netWorths.push(assets - debts);
-      allReportData.labels.push(localizedMonthAndYear(lastMonth));
+      allReportData.labels.push(
+        showDaily ? lastDate.toISOString() : localizedMonthAndYear(lastDate)
+      );
     }
 
     transactions.forEach(transaction => {
-      const transactionMonth = transaction
-        .get('date')
-        .clone()
-        .startOfMonth();
-      if (lastMonth === null) {
-        lastMonth = transactionMonth;
+      const transactionDate = showDaily
+        ? transaction.get('date').clone()
+        : transaction
+            .get('date')
+            .clone()
+            .startOfMonth();
+      if (lastDate === null) {
+        lastDate = transactionDate;
       }
 
-      // we're on a new month
-      if (transactionMonth.toISOString() !== lastMonth.toISOString()) {
+      // we're on a new month or day
+      if (transactionDate.toISOString() !== lastDate.toISOString()) {
         pushCurrentAccountData();
-        lastMonth = transactionMonth;
+        lastDate = transactionDate;
       }
 
       const transactionAccountId = transaction.get('accountId');
@@ -168,8 +193,9 @@ export class NetWorthComponent extends React.Component {
     });
 
     if (
-      lastMonth &&
-      allReportData.labels[allReportData.labels.length - 1] !== localizedMonthAndYear(lastMonth)
+      lastDate &&
+      allReportData.labels[allReportData.labels.length - 1] !==
+        (showDaily ? lastDate.toISOString() : localizedMonthAndYear(lastDate))
     ) {
       pushCurrentAccountData();
     }
@@ -178,34 +204,54 @@ export class NetWorthComponent extends React.Component {
     const { fromDate, toDate } = this.props.filters.dateFilter;
     if (transactions.length) {
       let currentIndex = 0;
-      const transactionMonth = transactions[0]
-        .get('date')
-        .clone()
-        .startOfMonth();
-      const lastFilterMonth = toDate
-        .clone()
-        .addMonths(1)
-        .startOfMonth();
-      while (transactionMonth.isBefore(lastFilterMonth)) {
-        if (!allReportData.labels.includes(localizedMonthAndYear(transactionMonth))) {
+      const transactionDate = showDaily
+        ? transactions[0].get('date').clone()
+        : transactions[0]
+            .get('date')
+            .clone()
+            .startOfMonth();
+      const lastFilterDate = showDaily
+        ? toDate.clone().addDays(1)
+        : toDate
+            .clone()
+            .addMonths(1)
+            .startOfMonth();
+      while (transactionDate.isBefore(lastFilterDate)) {
+        if (
+          !allReportData.labels.includes(
+            showDaily ? transactionDate.toISOString() : localizedMonthAndYear(transactionDate)
+          )
+        ) {
           const { assets, debts, netWorths, labels } = allReportData;
-          labels.splice(currentIndex, 0, localizedMonthAndYear(transactionMonth));
+          labels.splice(
+            currentIndex,
+            0,
+            showDaily ? transactionDate.toISOString() : localizedMonthAndYear(transactionDate)
+          );
           assets.splice(currentIndex, 0, assets[currentIndex - 1] || 0);
           debts.splice(currentIndex, 0, debts[currentIndex - 1] || 0);
           netWorths.splice(currentIndex, 0, netWorths[currentIndex - 1] || 0);
         }
 
         currentIndex++;
-        transactionMonth.addMonths(1);
+        if (showDaily) {
+          transactionDate.addDays(1);
+        } else {
+          transactionDate.addMonths(1);
+        }
       }
     }
 
     // Net Worth is calculated from the start of time so we need to handle "filters" here
     // rather than using `filteredTransactions` from context.
     const { labels, assets, debts, netWorths } = allReportData;
-    let startIndex = labels.findIndex(label => label === localizedMonthAndYear(fromDate));
+    let startIndex = labels.findIndex(
+      label => label === (showDaily ? fromDate.toISOString() : localizedMonthAndYear(fromDate))
+    );
     startIndex = startIndex === -1 ? 0 : startIndex;
-    let endIndex = labels.findIndex(label => label === localizedMonthAndYear(toDate));
+    let endIndex = labels.findIndex(
+      label => label === (showDaily ? toDate.toISOString() : localizedMonthAndYear(toDate))
+    );
     endIndex = endIndex === -1 ? labels.length + 1 : endIndex + 1;
 
     const filteredLabels = labels.slice(startIndex, endIndex);
